4900c57a8ab5112be65c57d5e4bb29a3096cd3c5
RakshithBhat03 <rakshith6666@gmail.com>
2023-01-25 23:01:28 +0530

    [feat]: add constants

---
+++ src/constants/constants.js
@@ -0,0 +1,38 @@
+export const ARROWUP = "ArrowUp";
+export const ARROWDOWN = "ArrowDown";
+export const ARROWLEFT = "ArrowLeft";
+export const ARROWRIGHT = "ArrowRight";
+export const MOVELEFT = "MOVELEFT";
+export const MOVERIGHT = "MOVERIGHT";
+export const MOVEUP = "MOVEUP";
+export const MOVEDOWN = "MOVEDOWN";
+export const RESET_GRID = "RESET_GRID";
+export const GAME_END = "GAME_END";
+export const RANDOM_FOOD_POSITION = "RANDOM_FOOD_POSITION";
+export const SHOWFOOD = "SHOWFOOD";
+export const RANDOM_KEY = "RANDOM_KEY";
+export const GROW_SNAKE = "GROW_SNAKE";
+export const START_GAME = "START_GAME";
+export const UPDATE_SCORE = "UPDATE_SCORE";
+export const INITIALSNAKEPOSITION = [
+  {
+    positionX: 4,
+    positionY: 4,
+  },
+  {
+    positionX: 5,
+    positionY: 4,
+  },
+  {
+    positionX: 6,
+    positionY: 4,
+  },
+  {
+    positionX: 7,
+    positionY: 4,
+  },
+  {
+    positionX: 8,
+    positionY: 4,
+  },
+];
